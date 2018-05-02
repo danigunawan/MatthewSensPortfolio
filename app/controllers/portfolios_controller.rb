ae0ee0a9e845849aec0eb93fea2087e0e0b5ebdc
misraelson <misraelson@gmail.com>
2018-05-02 15:04:19 -0700

    added update function to portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -20,4 +20,21 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    # whatever portfolio item id is it will find it
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
 end
