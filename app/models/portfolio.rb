bbf506beb433af1eb3d032edef30bb1e06fe21b5
misraelson <misraelson@gmail.com>
2018-05-02 19:59:25 -0700

    integrate concerns

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -13,7 +14,7 @@ class Portfolio < ApplicationRecord
   def set_defaults
     # double pipes with equal sign is a shortcut for saying if self.main_image == nil THEN set value to "http://placehold.it/600x400"
     # second way of setting our defaults other is via the migration default:
-    self.main_image ||= "http://placehold.it/600x400"
-    self.thumb_image ||= "http://placehold.it/350x200"
+    self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
+    self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
   end
 end
