ee4deec3bdeff7caedc7c607f1c15b2e0b6adbb1
misraelson <misraelson@gmail.com>
2018-05-02 19:47:30 -0700

    implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,5 +5,15 @@ class Portfolio < ApplicationRecord
     where(subtitle: 'ANGULAR')
   end
 
-  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') } 
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
+#this callback that occurs after the NEW action is called and automatically runs
+  after_initialize :set_defaults
+
+  def set_defaults
+    # double pipes with equal sign is a shortcut for saying if self.main_image == nil THEN set value to "http://placehold.it/600x400"
+    # second way of setting our defaults other is via the migration default:
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
