ef84d9a2d17f4f6577ea38dc9d27b29dbd4f6571
misraelson <misraelson@gmail.com>
2018-05-04 15:38:28 -0700

    integrated nested attributes for portfolio model

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,3 +46,11 @@ end
 end
 
 puts "9 portfolio items made!"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+  name: "Technology #{technology}",
+  portfolio_id: Portfolio.last.id
+  )
+end
+puts "3 technology items made!"
