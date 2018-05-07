40e72319f72c7c52a9a141303c31c0ab61ccb4f7
misraelson <misraelson@gmail.com>
2018-05-07 16:52:28 -0700

    refactored app controller to include concern for source feature

---
+++ app/controllers/concerns/set_source.rb
@@ -0,0 +1,13 @@
+module SetSource
+  # extending ActiveSupport allows us to have included do and other stuff
+  extend ActiveSupport:Concern
+
+  included do
+    before_action :set_source
+  end
+
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
+
+end
