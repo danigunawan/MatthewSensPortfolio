1f5dc0b44bbb275be75a856f53f65d75a22d4796
misraelson <misraelson@gmail.com>
2018-05-07 15:45:20 -0700

    completed authentication with virtual attributes for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+# below we use split.first and split.last so that if a user only enters a first
+# name the method still works. if we tried to use [0] and [1] we would get an error
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
