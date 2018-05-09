470c729819f3f95333b6ca4ab8530e9346e91a4e
misraelson <misraelson@gmail.com>
2018-05-08 18:42:31 -0700

    refactor our null object pattern for a guest user to be able to work with petergate

---
+++ app/models/guest_user.rb
@@ -0,0 +1,4 @@
+class GuestUser < User
+  # attr_accessor provides getters and setters; provides ability for this class to have data passed to it
+  attr_accessor :name, :first_name, :last_name, :email
+end
