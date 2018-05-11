6fdb5e9b4f5426e376aca61d1e8bc06e4451fd1f
misraelson <misraelson@gmail.com>
2018-05-10 18:27:06 -0700

    implemented refactor for navigation and active status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def login_helper style = ""
      if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
-      " ".html_safe +
+      "&nbsp;".html_safe +
       (link_to "Login", new_user_session_path, class: style)
      else
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
@@ -24,4 +24,53 @@ module ApplicationHelper
     SensPortfolioViewTool::Renderer.copyright 'Matthew Israelson', 'All rights reserved'
   end
 
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+
+  #style is the css style
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}<a/></#{tag_type}>" + "&nbsp;"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
 end
+
+# nav_links = <<NAV
+# <#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home<a/></#{tag_type}>
+# <#{tag_type}><a href="#{about_me_path}" class="#{style} #{active? about_me_path}">About Me<a/></#{tag_type}>
+# <#{tag_type}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact<a/></#{tag_type}>
+# <#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blog<a/></#{tag_type}>
+# <#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolio<a/></#{tag_type}>
+# NAV
+#   nav_links.html_safe
