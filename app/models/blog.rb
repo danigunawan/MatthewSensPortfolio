7e362d7765a7abdd146e4de5f679a6c16ceb10eb
misraelson <misraelson@gmail.com>
2018-09-03 18:27:22 -0700

    changed picture on home page

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,6 +6,6 @@ class Blog < ApplicationRecord
   validates_presence_of :title, :body, :topic_id
 
   belongs_to :topic
-
   has_many :comments, dependent: :destroy
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root to: 'pages#home'
+
   resources :topics, only: [:index, :show]
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
@@ -22,5 +24,4 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
 
-  root to: 'pages#home'
 end
